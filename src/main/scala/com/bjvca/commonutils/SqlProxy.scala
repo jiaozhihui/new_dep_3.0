0beb7dffe084146c68a4d0acb2b5c6ad13151813
jiao <jiaozhihui@bjvca.com>
2020-09-27 12:08:01 +0800

    3.0 多标签合成bug修复，可正常使用，任务队列完成，可部署在服务器上

---
+++ src/main/scala/com/bjvca/commonutils/SqlProxy.scala
@@ -0,0 +1,52 @@
+package com.bjvca.commonutils
+
+import java.sql.{Connection, PreparedStatement, ResultSet}
+
+trait QueryCallback {
+  def process(rs: ResultSet)
+}
+
+class SqlProxy {
+  private var rs: ResultSet = _
+  private var psmt: PreparedStatement = _
+
+  /**
+    * 执行修改语句
+    */
+  def executeUpdate(conn: Connection, sql: String, params: Array[Any]): Int = {
+    var rtn = 0
+    try {
+      psmt = conn.prepareStatement(sql)
+      if (params != null && params.length > 0) {
+        for (i <- params.indices) {
+          psmt.setObject(i + 1, params(i))
+        }
+      }
+      rtn = psmt.executeUpdate()
+    } catch {
+      case e: Exception => e.printStackTrace()
+    }
+    rtn
+  }
+
+  /**
+    * 执行查询语句
+    */
+  def executeQuery(conn: Connection, sql: String, params: Array[Any], queryCallback: QueryCallback): Unit = {
+        rs = null
+        try {
+          psmt = conn.prepareStatement(sql)
+          if (params != null && params.length > 0) {
+            for (i <- params.indices) {
+              psmt.setObject(i + 1, params(i))
+            }
+          }
+          rs = psmt.executeQuery()
+      queryCallback.process(rs)
+    } catch {
+      case e: Exception => e.printStackTrace()
+    }
+  }
+
+  def shutdown(conn: Connection): Unit = DataSourceUtil.closeResource(rs, psmt, conn)
+}
