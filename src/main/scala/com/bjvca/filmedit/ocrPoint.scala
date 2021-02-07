609b359377c44bc2da998524d79ecdab126bb18e
jiao <jiaozhihui@bjvca.com>
2021-02-07 11:39:15 +0800

    多标签卡住修复完毕

--- src/main/scala/com/bjvca/filmedit/ocrPoint.scala
+++ src/main/scala/com/bjvca/filmedit/ocrPoint.scala
@@ -323,177 +323,190 @@ object ocrPoint extends Logging {
 //      .show(1000,false)
       .createOrReplaceTempView("target_mul")
 
-
-    spark.sql(
+    val l = spark.sql(
       """
-        |select tpl_id,
-        |       label_id,
-        |       kukai_videos.videoName media_name,
-        |       lines_end - lines_start string_time_long,
-        |       total_duration_min*1000 totalLong,
-        |       concat_ws('_', lines_start, lines_end) string_time,
-        |       project_id,
-        |       media_id string_vid
-        |from recognition2_ocr
-        |join kukai_videos
-        |on videoId = media_id
-        |join target_mul
-        |on recognition2_ocr.OCR_content rlike target_mul.ocr
+        |select *
+        |from target_mul
         |""".stripMargin)
-      .createOrReplaceTempView("ranked")
-//        .show(1100,false)
+      .count()
+
+    if (l != 0) {
+
+      println(l)
+
+      spark.sql(
+        """
+          |select tpl_id,
+          |       label_id,
+          |       kukai_videos.videoName media_name,
+          |       lines_end - lines_start string_time_long,
+          |       total_duration_min*1000 totalLong,
+          |       concat_ws('_', lines_start, lines_end) string_time,
+          |       project_id,
+          |       media_id string_vid
+          |from recognition2_ocr
+          |join kukai_videos
+          |on videoId = media_id
+          |join target_mul
+          |on recognition2_ocr.OCR_content rlike target_mul.ocr
+          |""".stripMargin)
+        .createOrReplaceTempView("ranked")
+      //        .show(1100,false)
 
 
-    spark.sql(
-      """
-        |select *,row_number() OVER (PARTITION BY tpl_id,label_id ORDER BY label_id DESC) rank
-        |from ranked
-        |""".stripMargin)
-//            .show(1000,false)
-      .createOrReplaceTempView("resolution_ranked")
-    spark.sql(
-      """
-        |select tpl_id,min(max) shortSlab
-        |from (
-        |   select tpl_id,label_id,max(rank) max
-        |   from resolution_ranked
-        |   group by tpl_id,label_id) t1
-        |group by tpl_id
-        |""".stripMargin)
-//            .show(false)
-      .createOrReplaceTempView("shortSlab")
+      spark.sql(
+        """
+          |select *,row_number() OVER (PARTITION BY tpl_id,label_id ORDER BY label_id DESC) rank
+          |from ranked
+          |""".stripMargin)
+        //            .show(1000,false)
+        .createOrReplaceTempView("resolution_ranked")
+      spark.sql(
+        """
+          |select tpl_id,min(max) shortSlab
+          |from (
+          |   select tpl_id,label_id,max(rank) max
+          |   from resolution_ranked
+          |   group by tpl_id,label_id) t1
+          |group by tpl_id
+          |""".stripMargin)
+        //            .show(false)
+        .createOrReplaceTempView("shortSlab")
 
-    spark.sql(
-      """
-        |select resolution_ranked.*,shortSlab
-        |from resolution_ranked
-        |join shortSlab
-        |on resolution_ranked.tpl_id = shortSlab.tpl_id
-        |where rank <= shortSlab
-        |""".stripMargin)
-      .createOrReplaceTempView("shortSlabed")
-//            .show(1000,false)
+      spark.sql(
+        """
+          |select resolution_ranked.*,shortSlab
+          |from resolution_ranked
+          |join shortSlab
+          |on resolution_ranked.tpl_id = shortSlab.tpl_id
+          |where rank <= shortSlab
+          |""".stripMargin)
+        .createOrReplaceTempView("shortSlabed")
+      //            .show(1000,false)
 
-    val rstDF = spark.sql(
-      """
-        |select tpl_id,
-        |       label_id,
-        |       rank pid,
-        |       media_name,
-        |       string_time,
-        |       string_time_long,
-        |       string_vid,
-        |       shortSlab,
-        |       totalLong,
-        |       project_id
-        |from shortSlabed
-        |""".stripMargin)
-    rstDF.createOrReplaceTempView("rst")
-//                .show(100,false)
+      val rstDF = spark.sql(
+        """
+          |select tpl_id,
+          |       label_id,
+          |       rank pid,
+          |       media_name,
+          |       string_time,
+          |       string_time_long,
+          |       string_vid,
+          |       shortSlab,
+          |       totalLong,
+          |       project_id
+          |from shortSlabed
+          |""".stripMargin)
+      rstDF.createOrReplaceTempView("rst")
+      //                .show(100,false)
 
 
-    rstDF.foreachPartition(iterator => {
+      rstDF.foreachPartition(iterator => {
 
-      var conn: Connection = null
-      var ps: PreparedStatement = null
+        var conn: Connection = null
+        var ps: PreparedStatement = null
 
-      try {
-        conn = DriverManager.getConnection("jdbc:mysql://vcasltdb.mysql.rds.aliyuncs.com:3306/video_wave?serverTimezone=GMT%2B8", "video_cut_user", "Slt_2020")
-        conn.setAutoCommit(false)
+        try {
+          conn = DriverManager.getConnection("jdbc:mysql://vcasltdb.mysql.rds.aliyuncs.com:3306/video_wave?serverTimezone=GMT%2B8", "video_cut_user", "Slt_2020")
+          conn.setAutoCommit(false)
 
-        ps = conn.prepareStatement(
-          """INSERT INTO clip_tpl_result(`id`,`tpl_id`,`label_id`,`pid`,`media_name`,`string_time`,`string_time_long`,`string_vid`,`project_id`)
-            |VALUES(null,?,?,?,?,?,?,?,?)
-            |""".stripMargin)
-        var row = 0
-        iterator.foreach(it => {
-          ps.setInt(1, it.getAs[Int]("tpl_id"))
-          ps.setString(2, it.getAs[String]("label_id"))
-          ps.setInt(3, it.getAs[Int]("pid"))
-          ps.setString(4, it.getAs[String]("media_name"))
-          ps.setString(5, it.getAs[String]("string_time"))
-          ps.setInt(6, it.getAs[Int]("string_time_long"))
-          ps.setString(7, it.getAs[String]("string_vid"))
-          ps.setString(8, it.getAs[String]("project_id"))
+          ps = conn.prepareStatement(
+            """INSERT INTO clip_tpl_result(`id`,`tpl_id`,`label_id`,`pid`,`media_name`,`string_time`,`string_time_long`,`string_vid`,`project_id`)
+              |VALUES(null,?,?,?,?,?,?,?,?)
+              |""".stripMargin)
+          var row = 0
+          iterator.foreach(it => {
+            ps.setInt(1, it.getAs[Int]("tpl_id"))
+            ps.setString(2, it.getAs[String]("label_id"))
+            ps.setInt(3, it.getAs[Int]("pid"))
+            ps.setString(4, it.getAs[String]("media_name"))
+            ps.setString(5, it.getAs[String]("string_time"))
+            ps.setInt(6, it.getAs[Int]("string_time_long"))
+            ps.setString(7, it.getAs[String]("string_vid"))
+            ps.setString(8, it.getAs[String]("project_id"))
 
-          ps.addBatch()
-          row = row + 1
-          if (row % 1000 == 0) {
+            ps.addBatch()
+            row = row + 1
+            if (row % 1000 == 0) {
+              ps.executeBatch()
+              row = 0
+            }
+          })
+          if (row > 0)
             ps.executeBatch()
-            row = 0
+          conn.commit()
+
+        } catch {
+          case e: Exception => e.printStackTrace()
+        } finally {
+          if (ps != null) {
+            ps.close()
+          }
+          if (conn != null) {
+            conn.close()
+          }
+        }
+      })
+      spark.sql(
+        """
+          |select tpl_id,max(rst.pid) num
+          |from rst
+          |group by tpl_id
+          |""".stripMargin)
+        .createOrReplaceTempView("numTab")
+      //          .show(100,false)
+
+
+      spark.sql(
+        """
+          |select *
+          |from numTab
+          |""".stripMargin)
+        .toJSON
+        .rdd
+        .foreach(str => {
+          val nObject = JSON.parseObject(str)
+          val tpl_id = nObject.getString("tpl_id")
+          val num = nObject.getString("num")
+
+          val sqlProxy = new SqlProxy()
+          val client = DataSourceUtil.getConnection
+          try {
+            sqlProxy.executeUpdate(client, "update clip_tpl set num=? where tpl_id=?",
+              Array(num, tpl_id))
+          }
+          catch {
+            case e: Exception => e.printStackTrace()
+          } finally {
+            sqlProxy.shutdown(client)
           }
         })
-        if (row > 0)
-          ps.executeBatch()
-        conn.commit()
 
-      } catch {
-        case e: Exception => e.printStackTrace()
-      } finally {
-        if (ps != null) {
-          ps.close()
-        }
-        if (conn != null) {
-          conn.close()
-        }
-      }
-    })
-    spark.sql(
-      """
-        |select tpl_id,max(rst.pid) num
-        |from rst
-        |group by tpl_id
-        |""".stripMargin)
-      .createOrReplaceTempView("numTab")
-    //          .show(100,false)
+    }
 
+      spark.sql(
+        """
+          |select *
+          |from clip_task_mul
+          |""".stripMargin)
+        .rdd
+        .foreach(x => {
+          val tpl_id = x.getInt(0)
 
-    spark.sql(
-      """
-        |select *
-        |from numTab
-        |""".stripMargin)
-      .toJSON
-      .rdd
-      .foreach(str => {
-        val nObject = JSON.parseObject(str)
-        val tpl_id = nObject.getString("tpl_id")
-        val num = nObject.getString("num")
+          val sqlProxy = new SqlProxy()
+          val client = DataSourceUtil.getConnection
+          try {
+            sqlProxy.executeUpdate(client, "update `clip_tpl` set status=1 where tpl_id = ?",
+              Array(tpl_id))
+          }
+          catch {
+            case e: Exception => e.printStackTrace()
+          } finally {
+            sqlProxy.shutdown(client)
+          }
 
-        val sqlProxy = new SqlProxy()
-        val client = DataSourceUtil.getConnection
-        try {
-          sqlProxy.executeUpdate(client, "update clip_tpl set num=? where tpl_id=?",
-            Array(num, tpl_id))
-        }
-        catch {
-          case e: Exception => e.printStackTrace()
-        } finally {
-          sqlProxy.shutdown(client)
-        }
-      })
-    spark.sql(
-      """
-        |select *
-        |from clip_task_mul
-        |""".stripMargin)
-      .rdd
-      .foreach(x => {
-        val tpl_id = x.getInt(0)
-
-        val sqlProxy = new SqlProxy()
-        val client = DataSourceUtil.getConnection
-        try {
-          sqlProxy.executeUpdate(client, "update `clip_tpl` set status=1 where tpl_id = ?",
-            Array(tpl_id))
-        }
-        catch {
-          case e: Exception => e.printStackTrace()
-        } finally {
-          sqlProxy.shutdown(client)
-        }
-
-      })
+        })
 
 
 
