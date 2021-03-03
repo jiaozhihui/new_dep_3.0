1eaab478fcf4a78c96e52e87013b1f18e862789f
jiao <jiaozhihui@bjvca.com>
2021-03-03 14:27:59 +0800

    AllCleand10: 合成前清洗

--- src/main/scala/com/bjvca/videocut/AllCleand10.scala
+++ src/main/scala/com/bjvca/videocut/AllCleand10.scala
@@ -1,11 +1,12 @@
 package com.bjvca.videocut
 
+import java.sql.{Connection, DriverManager, PreparedStatement}
 import java.text.SimpleDateFormat
 import java.util.Date
 
 import com.alibaba.fastjson.{JSON, JSONArray, JSONObject}
 import com.bjvca.bean.{Story1, Story2, Story3}
-import com.bjvca.commonutils.{ConfUtils, DataSourceUtil, SqlProxy}
+import com.bjvca.commonutils.{ConfUtils, ConfigurationManager, DataSourceUtil, SqlProxy}
 import org.apache.spark.internal.Logging
 import org.apache.spark.sql.SparkSession
 import org.elasticsearch.spark._
@@ -15,15 +16,14 @@ import scala.collection.mutable.ListBuffer
 
 /**
  * copy by AllCleand9
+ * 加入数据清洗逻辑
  */
 object AllCleand10 extends Logging {
 
   def main(args: Array[String]): Unit = {
 
 
-    logWarning("开始合成")
-
-    val confUtil = new ConfUtils("application.conf")
+    val confUtil = new ConfUtils("application58.conf")
     //    val confUtil = new ConfUtils("线上application.conf")
 
     // 创建sparkSession
@@ -178,8 +178,225 @@ object AllCleand10 extends Logging {
         .load()
         .createOrReplaceTempView("recognition2_ocr")
 
-      //    join task
-      //      on task.video_id = media_id
+      // 9.台词清洗偏移量
+      spark.read.format("jdbc")
+        .options(Map("url" -> s"jdbc:mysql://${confUtil.videocutMysqlHost}:3306/video_wave?characterEncoding=utf-8&useSSL=false",
+          "driver" -> "com.mysql.jdbc.Driver",
+          "user" -> confUtil.videocutMysqlUser,
+          "password" -> confUtil.videocutMysqlPassword,
+          "dbtable" -> "ocr_offset"
+        ))
+        .load()
+        .createOrReplaceTempView("ocr_offset")
+
+      /**
+       * 开始清洗逻辑
+       */
+      logWarning("开始清洗台词")
+
+      // 拿到之前偏移量
+      var last_offset = 0
+      spark.sql(
+        """
+          |select offset
+          |from ocr_offset
+          |""".stripMargin)
+        .collect().foreach(row => {
+        last_offset = row.getInt(0)
+      })
+
+      // 获取最大偏移量 max_offset
+      var max_offset = 0
+      spark.sql(
+        """
+          |select max(id) max_offset
+          |from recognition2_ocr
+          |""".stripMargin)
+        //        .createOrReplaceTempView("cur_offset")
+        .collect.foreach(row => {
+        max_offset = row.getInt(0)
+      })
+
+      // 如果有清洗任务，执行清洗任务，否则不执行
+      if (last_offset != max_offset) {
+
+        // 从offset处读取数据,去中文和空格
+        spark.sql(
+          s"""
+             |select id,platform_id,project_id,media_id,lines_start,lines_end,condfid,trim(regexp_replace(OCR_content, '[a-zA-Z\\pP‘’“”]+', '')) OCR_content,offset
+             |from recognition2_ocr
+             |where id > $last_offset
+             |""".stripMargin)
+          //      .show(100,false)
+          .createOrReplaceTempView("ocr")
+
+
+        spark.udf.register("get_similar", (str1: String, str2: String) => {
+
+          val array1 = if (null != str1) {
+            str1.toCharArray.filter(ch => ch != ' ')
+          } else {
+            Array.empty
+          }
+          val array2 = if (null != str2) {
+            str2.toCharArray.filter(ch => ch != ' ')
+          } else {
+            Array.empty
+          }
+          val inter_chars = array1.intersect(array2)
+          val i = (inter_chars.length.toDouble / (array1.length + array2.length)).toFloat
+          i * 2
+
+        })
+
+        val noSame = spark.sql(
+          """
+            |select *,ROW_NUMBER() OVER (ORDER BY t1.id ASC) row_num
+            |from
+            | (select t1.id,t1.platform_id,t1.project_id,t1.media_id,t1.lines_start,t1.lines_end,t1.OCR_content,t1.condfid,get_similar(t1.OCR_content,t2.OCR_content) similar
+            | from ocr t1
+            | left join ocr t2
+            | on t1.id = t2.id - 1
+            | order by t1.id) T1
+            |where T1.similar < 0.7
+            |""".stripMargin)
+
+        noSame.createOrReplaceTempView("noSame")
+        //      .show(100,false)
+
+        // id_span:0->上一条没有被删除，!0->前续数据被删除的条数
+        spark.sql(
+          """
+            |select t1.*,nvl(t1.id - t2.id - 1,0) id_span
+            |from noSame t1
+            |left join noSame t2
+            |on t1.row_num = t2.row_num + 1
+            |""".stripMargin)
+          .createOrReplaceTempView("id_span")
+        //      .show(100,false)
+
+        spark.sql(
+          """
+            |select t1.id,
+            |       t1.platform_id,
+            |       t1.project_id,
+            |       t1.media_id,
+            |       ocr.lines_start,
+            |       t1.lines_end,
+            |       if(t1.condfid > ocr.condfid,t1.OCR_content,ocr.OCR_content) OCR_content,
+            |       if(t1.condfid > ocr.condfid,t1.condfid,ocr.condfid) condfid
+            |from
+            | (select *,id - id_span tar_id
+            | from id_span
+            | where id_span != 0) t1
+            |join ocr
+            |on tar_id = ocr.id
+            |order by t1.id
+            |""".stripMargin)
+          .createOrReplaceTempView("rep_etl")
+        //      .show(100,false)
+
+        spark.sql(
+          """
+            |select id,
+            |       platform_id,
+            |       project_id,
+            |       media_id,
+            |       lines_start,
+            |       lines_end,
+            |       OCR_content,
+            |       condfid
+            |from id_span
+            |where id_span = 0
+            |""".stripMargin)
+          .createOrReplaceTempView("normal")
+        //      .show(100,false)
+
+        spark.sql(
+          """
+            |select *
+            |from normal
+            |union all
+            |select *
+            |from rep_etl
+            |""".stripMargin)
+          .where("length(OCR_content) > 0")
+          .orderBy("id")
+          //      .show(100,false)
+
+
+          .foreachPartition(iterator => {
+
+            var conn: Connection = null
+            var ps: PreparedStatement = null
+
+            try {
+              conn = DriverManager.getConnection(ConfigurationManager.getProperty("jdbc.url"), "video_cut_user", "Slt_2020")
+              conn.setAutoCommit(false)
+
+              ps = conn.prepareStatement(
+                """INSERT INTO recognition2_ocr_etl(id,platform_id,project_id,media_id,lines_start,lines_end,OCR_content,condfid)
+                  |VALUES(?,?,?,?,?,?,?,?)
+                  |""".stripMargin)
+              var row = 0
+              iterator.foreach(it => {
+                ps.setInt(1, it.getAs[Int]("id"))
+                ps.setString(2, it.getAs[String]("platform_id"))
+                ps.setString(3, it.getAs[String]("project_id"))
+                ps.setString(4, it.getAs[String]("media_id"))
+                ps.setInt(5, it.getAs[Int]("lines_start"))
+                ps.setInt(6, it.getAs[Int]("lines_end"))
+                ps.setString(7, it.getAs[String]("OCR_content"))
+                ps.setString(8, it.getAs[String]("condfid"))
+
+                ps.addBatch()
+                row = row + 1
+                if (row % 1000 == 0) {
+                  ps.executeBatch()
+                  row = 0
+                }
+              })
+              if (row > 0)
+                ps.executeBatch()
+              conn.commit()
+
+
+              // 更改偏移量
+              val sqlProxy = new SqlProxy()
+              val client = DataSourceUtil.getConnection
+              try {
+                sqlProxy.executeUpdate(client, "update ocr_offset set offset=?",
+                  Array(max_offset))
+              }
+              catch {
+                case e: Exception => e.printStackTrace()
+              } finally {
+                sqlProxy.shutdown(client)
+              }
+
+
+            } catch {
+              case e: Exception => e.printStackTrace()
+            } finally {
+              if (ps != null) {
+                ps.close()
+              }
+              if (conn != null) {
+                conn.close()
+              }
+            }
+          })
+
+      } else {
+        logWarning("当前无台词清洗任务")
+      }
+
+      /**
+       * 开始标签合成
+       */
+
+      logWarning("开始合成")
+
       // 1.拿到所有的广告位 aaa
       spark.sql(
         """
@@ -412,6 +629,38 @@ object AllCleand10 extends Logging {
         "es.nodes" -> confUtil.adxStreamingEsHost,
         "es.port" -> "9200"
       ))
+
+//      // 存储一份带有字幕的数据到ES
+//      rst.map(_.asInstanceOf[JSONObject])
+//          .mapPartitions(iter => {
+//            var conn: Connection = null
+//            var ps: PreparedStatement = null
+//
+//            try {
+//              conn = DataSourceUtil.getConnection
+//              ps = conn.prepareStatement(
+//                """
+//                  |select lines_start,lines_end,OCR_content from recognition2_ocr_etl where media_id = ? and lines_start > ? and lines_end < ?
+//                  |""".stripMargin)
+//              iter.foreach(it => {
+//                ps.setString(1,it.getString("string_vid"))
+//                ps.setLong(2,it.getString("b_t").toLong)
+//                ps.setLong(3,it.getString("e_t").toLong)
+//
+//                val resultSet = ps.executeQuery()
+//                resultSet
+//              })
+//            } catch {
+//            } finally {
+//              if (ps != null) {
+//                ps.close()
+//              }
+//              if (conn != null) {
+//                conn.close()
+//              }
+//            }
+//
+//          })
 
       logWarning("多标签存入ES成功")
 
